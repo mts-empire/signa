aa4dfebce237119f21f059c89f22fd55cbdd13f3
mts-empire <mtsatsocial@gmail.com>
2026-08-06 02:52:47 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        darkBg: '#0b0f17',
-        cardBg: '#131b2e',
-        accentBlue: '#3b82f6',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
